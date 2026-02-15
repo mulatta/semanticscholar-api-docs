cfb2a41600e15fb96df220642b28b74c3cbc3227
mulatta <67085791+mulatta@users.noreply.github.com>
2026-02-16 01:40:36 +0900

    init: project scaffolding with flake-parts

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "PROJ_NAME";
+
+  inputs = {
+    # keep-sorted start
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    # keep-sorted end
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      imports = [
+        ./nix/formatter.nix
+        ./nix/shell.nix
+      ];
+
+      perSystem =
+        { system, ... }:
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
+        };
+    };
+}
